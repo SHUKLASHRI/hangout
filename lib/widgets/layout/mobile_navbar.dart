ad9ee0e1ba3099959d0f114ebe8b97d465c2e724
Shrinath Shukla <shrinathshukla6907@gmail.com>
2026-04-28 12:05:41 +0530

    Fix: Restore missing GoogleFonts import in MobileNavbar

--- lib/widgets/layout/mobile_navbar.dart
+++ lib/widgets/layout/mobile_navbar.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 import 'package:lucide_icons_flutter/lucide_icons.dart';
+import 'package:google_fonts/google_fonts.dart';
 import '../../providers/app_state.dart';
 import '../liquid_glass_card.dart';
 
