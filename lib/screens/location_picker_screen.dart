29ad635c2c6aaa657dcb9a6fab9e5b6a0c56e294
Shrinath Shukla <shrinathshukla6907@gmail.com>
2026-06-10 12:50:10 +0530

    Update Google Maps API key to new restricted key

--- lib/screens/location_picker_screen.dart
+++ lib/screens/location_picker_screen.dart
@@ -37,7 +37,7 @@ class _LocationPickerScreenState extends State<LocationPickerScreen> {
 
   Future<void> _fetchAddress(LatLng position) async {
     setState(() => _isLoadingAddress = true);
-    final apiKey = dotenv.env['GOOGLE_MAPS_API_KEY']?.isNotEmpty == true ? dotenv.env['GOOGLE_MAPS_API_KEY']! : 'AIzaSyBCnS6a-CIIKt71FaaIEigU1Wko51Tqtj0';
+    final apiKey = dotenv.env['GOOGLE_MAPS_API_KEY']?.isNotEmpty == true ? dotenv.env['GOOGLE_MAPS_API_KEY']! : 'AIzaSyAXG-Vh5-aatnwfSwS4Za4zs_HVXf3Jp64';
     final url = Uri.parse(
         'https://maps.googleapis.com/maps/api/geocode/json?latlng=${position.latitude},${position.longitude}&key=$apiKey');
 
